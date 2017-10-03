ae09b6f56d39ec393cdea287317c32c28241aa8e
tongqiu-jia <jiatongqiu@gmail.com>
2017-10-03 10:17:14 -0700

    add example results

---
+++ Example_Command_Line_Tools/example_runpyNBS.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+for cancer_type in 'BLCA' 'COAD' 'HNSC' 'OV' 'UCEC'
+do
+	#cancer_type='BLCA'
+	echo $cancer_type
+	pyNBS_script='/cellar/users/t1jia/pyNBS/Example_Command_Line_Tools/run_pyNBS.py'
+	network_file='/cellar/users/t1jia/pyNBS/data/Network_Files/CancerSubnetwork.txt'
+	mutation_file='/cellar/users/t1jia/Data/Cancer_Mutations/'$cancer_type'_sm_data.txt'
+
+	network_file_basename=${network_file##*/}
+	network_name=${network_file_basename%.*}
+
+	# K=3 for 'COAD', K=4 for 'BLCA', 'HNSC', 'OV', 'UCEC'
+	K=4
+	if [ "$cancer_type" == "COAD" ]
+		then
+		K=3
+	fi
+
+	clincal_file='/cellar/users/t1jia/pyNBS/data/Clinical_Files/'$cancer_type'.clin.merged.surv.txt'
+
+	result_dir='/cellar/users/t1jia/pyNBS/Example_Command_Line_Tools/results/'$cancer_type'/'
+	mkdir -p $result_dir
+	cc_mat_file=$result_dir$cancer_type'_'$network_name'_k_'$K'_cc_mat.csv'
+	cluster_assign_file=$result_dir$cancer_type'_'$network_name'_k_'$K'_cluster_assign.csv'
+	map_file=$result_dir$cancer_type'_'$network_name'_k_'$K'_map.png'
+	km_file=$result_dir$cancer_type'_'$network_name'_k_'$K'_km.png'
+
+	python $pyNBS_script $mutation_file $network_file -v -mf list -md '\t' -reg -n 100 -k $K -netNMF_n 250 -s_cc $cc_mat_file -s_ca $cluster_assign_file -s_map $map_file -km_plot True -clin $clincal_file -km_title $cancer_type' Survival Plot' -s_km $km_file
+
+done
--- pyNBS/pyNBS_plotting.py
+++ pyNBS/pyNBS_plotting.py
@@ -97,3 +97,58 @@ def cluster_KMplot(cluster_assign, clin_data_fn, title=None, lr_test=True, tmax=
     if lr_test:
         return p
     return
+
+
+# Function for plotting Kaplan Meier plot of cluster survivals
+# Requires lifelines package
+# Format of clin_data_fn is specific to the file used on Hofree et al analysis
+# clin_data_fn is the the clinical data of TCGA cohort from Broad Firehose
+# cluster_assign ias a pandas Series of the patient cluster assignments from NBS with patient ID's as the index
+# tmax is the maximum plot duration for the KMplot, but the logrank test always calculates to longest survival point
+def cluster_KMplot_Hofree(cluster_assign, clin_data_fn, title=None, lr_test=True, tmax=1825, save_path=None):
+    # Initialize KM plotter
+    kmf = KaplanMeierFitter()
+    # Load and format clinical data
+
+    clin_data = pd.read_csv(clin_data_fn, sep='\t', index_col=0)
+    clin_data = clin_data.T
+    clin_data.index = [index.upper() for index in clin_data.index]
+    surv = clin_data[['vital_status', 'days_to_death', 'days_to_last_followup']].fillna(0)
+    surv['overall_survival'] = surv[['days_to_death', 'days_to_last_followup']].max(axis=1).map(lambda x: int(x))
+    
+
+    # Number of clusters
+    clusters = sorted(list(cluster_assign.value_counts().index))
+    k = len(clusters)
+    # Initialize KM Plot Settings
+    fig = plt.figure(figsize=(10, 7)) 
+    ax = plt.subplot(1,1,1)
+    colors = sns.color_palette('hls', k)
+    cluster_cmap = {clusters[i]:colors[i] for i in range(k)}
+    # Plot each cluster onto KM Plot
+    for clust in clusters:
+        clust_pats = cluster_assign[cluster_assign==clust].index.values
+        clust_surv_data = surv.ix[clust_pats].dropna()
+        kmf.fit(clust_surv_data.overall_survival, clust_surv_data.vital_status, label='Group '+str(clust)+' (n=' +  str(len(clust_surv_data)) + ')')
+        kmf.plot(ax=ax, color=cluster_cmap[clust], ci_show=False)
+    # Set KM plot limits and labels
+    if tmax is not None:
+        plt.xlim((0,tmax))
+    plt.xlabel('Time (Days)', fontsize=16)
+    plt.ylabel('Survival Probability', fontsize=16)
+    # Multivariate logrank test
+    if lr_test:
+        cluster_survivals = pd.concat([surv, cluster_assign], axis=1).dropna().astype(int)
+        p = multiv_lr_test(np.array(cluster_survivals.overall_survival), 
+                           np.array(cluster_survivals[cluster_assign.name]), 
+                           event_observed=np.array(cluster_survivals.vital_status)).p_value
+        print 'Multi-Class Log-Rank P:', p
+        plt.title(title+'\np='+repr(round(p, 6)), fontsize=24, y=1.02)
+    else:
+        plt.title(title, fontsize=24, y=1.02)
+    # Save KM plot
+    if save_path is not None:
+        plt.savefig(save_path, bbox_inches='tight')
+    if lr_test:
+        return p
+    return
